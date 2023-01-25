5284da977154e9e8eea91c845038b52521c01b64
tigerskin <davidskhdiaz@gmail.com>
2023-01-25 11:46:56 -0600

    Co-authored-by: EVARISTO JUAN VELASCO HERNANDEZ <212241068@alumnos.utn.edu.mx>

---
+++ resources/views/login/login.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+    <meta name="description" content="">
+        <link rel="shortcut icon" href="img/logo.png" type="image/png">
+        <link rel="stylesheet" type="text/css"  href="/css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="/fonts/font-awesome/css/font-awesome.css">
+
+        <!--Links de la OnePage-->
+        <link rel="stylesheet" type="text/css"  href="../css/style.css">
+        <link rel="stylesheet" type="text/css" href="../css/nivo-lightbox/nivo-lightbox.css">
+        <link rel="stylesheet" type="text/css" href="../css/nivo-lightbox/default.css">
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800,600,300' rel='stylesheet' type='text/css'>
+        <script type="text/javascript" src="../js/modernizr.custom.js"></script>
+</head>
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+
+<nav id="menu" class="navbar navbar-default navbar-fixed-top">
+    <div class="container"> 
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only"></span> 
+            <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+            <a class="navbar-brand page-scroll" href="index.html"><i class=""></i> Purificadora La Inmaculada</a> </div>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#page-top" class="page-scroll">Home</a></li>
+                <li><a href="#about" class="page-scroll">Acerca de</a></li>
+                <li><a href="#portfolio" class="page-scroll">Catalogo de productos</a></li>
+                <li><a href="#contact" class="page-scroll">Contacto</a></li>
+                <li><a href="login.html" >Inicio de Sesion</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+
+<!-- Querys -->
+<script type="text/javascript" src="js/jquery.1.11.1.js"></script> 
+    <script type="text/javascript" src="js/bootstrap.js"></script> 
+    <script type="text/javascript" src="js/SmoothScroll.js"></script> 
+    <script type="text/javascript" src="js/nivo-lightbox.js"></script> 
+    <script type="text/javascript" src="js/jquery.isotope.js"></script> 
+    <script type="text/javascript" src="js/jqBootstrapValidation.js"></script> 
+    <script type="text/javascript" src="js/contact_me.js"></script> 
+    <script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
---
+++ resources/views/sitio/index.blade.php
@@ -0,0 +1,289 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Home</title>
+  <meta name="description" content="">
+      <link rel="shortcut icon" href="img/logo.png" type="image/png">
+      <link rel="stylesheet" type="text/css"  href="/css/bootstrap.css">
+      <link rel="stylesheet" type="text/css" href="/fonts/font-awesome/css/font-awesome.css">
+
+      <!--Links de la OnePage-->
+      <link rel="stylesheet" type="text/css"  href="/css/style.css">
+      <link rel="stylesheet" type="text/css" href="/css/nivo-lightbox/nivo-lightbox.css">
+      <link rel="stylesheet" type="text/css" href="/css/nivo-lightbox/default.css">
+      <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800,600,300' rel='stylesheet' type='text/css'>
+      <script type="text/javascript" src="../js/modernizr.custom.js"></script>
+</head>
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+<!-- NavBar-->
+  <nav id="menu" class="navbar navbar-default navbar-fixed-top">
+    <div class="container"> 
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"> <span class="sr-only"></span> 
+        <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
+        <a class="navbar-brand page-scroll" href="index.html"><i class=""></i> Purificadora La Inmaculada</a> </div>
+    
+    
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#page-top" class="page-scroll">Home</a></li>
+          <li><a href="#about" class="page-scroll">Acerca de</a></li>
+          <li><a href="#portfolio" class="page-scroll">Catalogo de productos</a></li>
+          <li><a href="#contact" class="page-scroll">Contacto</a></li>
+          <li><a href="login.html" >Inicio de Sesion</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+<!-- Header purificadora -->
+<header id="header">
+  <div class="intro">
+    <div class="container">
+      <div class="row">
+        <div class="intro-text">
+        <h1>Purificadora La Inmaculada</h1>
+          <p>Agua Alcalina • Agua destilada • Agua purificada</p>
+          <a href="#about" class="btn btn-custom btn-lg page-scroll">Leer Mas...</a>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+</header>
+<!-- Acerca de nosotros -->
+<div id="about">
+  <div class="container">
+    <div class="section-title text-center center">
+      <h2>Acerca de</h2>
+      <hr>
+    </div>
+    <div class="row">
+      <div class="col-xs-12 col-md-6"> <img src="img/logo.png" class="img-responsive" alt=""> </div>
+      <div class="col-xs-12 col-md-6">
+        <div class="about-text">
+          <p>Tras 9 años de esfuerzo y dedicación, en Agua Inmaculadael manantial hemos logrado posicionarnos como la franquicia #1 en fabricación, 
+            distribución  de agua, 
+            ofreciendo  a tu alcance una agua de calidad a tu hogar .</p>
+          <p>En México, somos la única empresa en el ramo que trabaja bajo el concepto de franquicia, por ello, todas las personas que deciden invertir 
+            en un negocio propio con nosotros, reciben el  respaldo de una
+            marca posicionada y una guía detallada con los pasos que deberán seguir para poder alcanzar el éxito. </p>
+          <a href="#portfolio" class="btn btn-default btn-lg page-scroll">Explora Nuestros productos</a> </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- Catalogo Productos -->
+<div id="portfolio">
+  <div class="container">
+    <div class="section-title text-center center">
+      <h2>Nuestros productos</h2>
+      <hr>
+    </div>
+    <div class="categories">
+      <ul class="cat">
+        <li>
+          <ol class="type">
+            <li><a href="#" data-filter="*" class="active">Todos los productos</a></li>
+            <li><a href="#" data-filter=".web">Tequilas</a></li>
+            <li><a href="#" data-filter=".photography">Whisky</a></li>
+            <li><a href="#" data-filter=".product">Cerveza</a></li>
+          </ol>
+        </li>
+      </ul>
+      <div class="clearfix"></div>
+    </div>
+    <div class="row">
+
+<!-- Tequilas -->
+      <div class="portfolio-items">
+        <div class="col-sm-6 col-md-3 col-lg-3 web">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/agua.jpeg" title="imagen" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>Imagen 1</h4>
+              </div>
+              <img src="img/portfolio/agua.jpeg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+
+
+        <div class="col-sm-6 col-md-3 col-lg-3  web">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/agua.jpeg" title="imagen" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>Imagen 2</h4>
+              </div>
+              <img src="img/portfolio/agua.jpeg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+
+
+        <div class="col-sm-6 col-md-3 col-lg-3 web">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/agua.jpeg" title="imagen" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>Imagen 3</h4>
+              </div>
+              <img src="img/portfolio/agua.jpeg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+
+
+        <div class="col-sm-6 col-md-3 col-lg-3 web">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/agua.jpeg" title="Imagen" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>Imagen 4</h4>
+              </div>
+              <img src="img/portfolio/agua.jpeg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+
+
+        <div class="col-sm-6 col-md-3 col-lg-3 web">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/agua.jpeg" title="Imagen" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>imagen 5</h4>
+              </div>
+              <img src="img/portfolio/agua.jpeg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+
+<!-- whiskys -->
+        <div class="col-sm-6 col-md-3 col-lg-3 photography">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/agua.jpeg" title="Imagen" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>Imagen 6</h4>
+              </div>
+              <img src="img/portfolio/agua.jpeg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+
+
+        <div class="col-sm-6 col-md-3 col-lg-3 photography">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/agua.jpeg" title="Imagen" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>Imagen 7</h4>
+              </div>
+              <img src="img/portfolio/agua.jpeg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+
+
+        <div class="col-sm-6 col-md-3 col-lg-3 photography">
+          <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/agua.jpeg" title="Imagen" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>Imagen 8</h4>
+              </div>
+              <img src="img/portfolio/agua.jpeg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+
+        <div class="col-sm-6 col-md-3 col-lg-3 photography">
+        <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/agua.jpeg" title="Imagen" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>Imagen 9</h4>
+              </div>
+              <img src="img/portfolio/agua.jpeg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+
+<!--Cervezas-->
+
+
+
+        <div class="col-sm-6 col-md-3 col-lg-3 product">
+        <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/agua.jpeg" title="Imagen" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>Imagen 10</h4>
+              </div>
+              <img src="img/portfolio/agua.jpeg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+
+
+        <div class="col-sm-6 col-md-3 col-lg-3 product">
+        <div class="portfolio-item">
+            <div class="hover-bg"> <a href="img/portfolio/agua.jpeg" title="Imagen" data-lightbox-gallery="gallery1">
+              <div class="hover-text">
+                <h4>Imagen 11</h4>
+              </div>
+              <img src="img/portfolio/agua.jpeg" class="img-responsive" alt="Project Title"> </a> </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Contacto -->
+
+
+<div id="contact" class="text-center">
+  <div class="container">
+    <div class="section-title center">
+      <h2>Envianos un comentario</h2>
+      <hr>
+    </div>
+    <div class="col-md-8 col-md-offset-2">
+      <form name="sentMessage" id="contactForm" novalidate>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <input type="text" id="name" class="form-control" placeholder="Nombre" required="required">
+              <p class="help-block text-danger"></p>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="form-group">
+              <input type="email" id="email" class="form-control" placeholder="Correo Electronico" required="required">
+              <p class="help-block text-danger"></p>
+            </div>
+          </div>
+        </div>
+        <div class="form-group">
+          <textarea name="message" id="message" class="form-control" rows="4" placeholder="Mensaje" required></textarea>
+          <p class="help-block text-danger"></p>
+        </div>
+        <div id="success"></div>
+        <button type="submit" class="btn btn-default btn-lg">Enviar Mensaje</button>
+      </form>
+      <div class="social">
+        <ul>
+          <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+          <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+          <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+          
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+<div id="footer">
+  <div class="container text-center">
+    <div class="fnav">
+      <p>Copyright &copy; 2022 La Inmaculada Purificadora. Desarollado por <a href="" rel="nofollow">IguanoSolutions</a>.</p>
+    </div>
+  </div>
+</div>
+<!-- Querys -->
+    <script type="text/javascript" src="js/jquery.1.11.1.js"></script> 
+    <script type="text/javascript" src="js/bootstrap.js"></script> 
+    <script type="text/javascript" src="js/SmoothScroll.js"></script> 
+    <script type="text/javascript" src="js/nivo-lightbox.js"></script> 
+    <script type="text/javascript" src="js/jquery.isotope.js"></script> 
+    <script type="text/javascript" src="js/jqBootstrapValidation.js"></script> 
+    <script type="text/javascript" src="js/contact_me.js"></script> 
+    <script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
